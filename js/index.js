ff1b22f588bba6e3befc0fe83407d4ca0b536e0e
EvgeniQwerty <evgeniqwerty1337@gmail.com>
2023-03-24 20:21:37 +0300

    Some changes in media queries

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
-import L from 'leaflet';
-
+//import L from 'leaflet';
 let map = L.map('map').setView([51.505, -0.09], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
